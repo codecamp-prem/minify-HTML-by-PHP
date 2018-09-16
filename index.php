ff50b9c07bd232bd011e3a034e6e8330b0ac07b9
Prem Bikram Limbu <prem.bikram.limbu@gmail.com>
2018-09-16 20:34:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -21,3 +21,7 @@ turpis egestas. <a href="#">Donec non enim</a> in turpis pulvinar facilisis.</p>
 ob_end_flush();
 
 ?>
+<!-- OUTPUT HTML in View Source -->
+<!--
+<h1>Lorem Ipsum</h1><p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. <a href="#">Donec non enim</a> in turpis pulvinar facilisis.</p><h2>Header Level 2</h2><ol><li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li><li>Aliquam tincidunt mauris eu risus.</li></ol><p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac </p> 
+-->
